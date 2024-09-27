8035184338e570a8b58cfd81bdf38bf3648bea6a
Barry Wu <barry@wukanglideMacBook-Pro.local>
2024-09-27 00:47:05 -0500

    Modify rand() to mt19937

--- HW02/task1.cpp
+++ HW02/task1.cpp
@@ -2,6 +2,7 @@
 #include "scan.h"
 #include <chrono>
 #include <vector>
+#include <random>
 
 using std::cout;
 using std::endl;
@@ -16,16 +17,19 @@ int main(int argc, char* argv[])
 
     //std::cout << "There are " << argc << " arguments:\n";
     int N = std::atoi(argv[1]);
-    std::srand(static_cast<unsigned>(std::time(0)));
+    // std::srand(static_cast<unsigned>(std::time(0)));
+    std::random_device entropy_source;
+    std::mt19937 generator(entropy_source());
+    std::uniform_real_distribution<float> dist(-1., 1.);
     
     float* nums = new float[N];
     float* output = new float[N];
 
     for (int i = 0; i < N; ++i)
     {
-        nums[i] = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 2.0)) - 1.0;
+        // nums[i] = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 2.0)) - 1.0;
         // nums[i] = i;
-
+        nums[i] = dist(generator);
     }
 
     start = high_resolution_clock::now();
--- HW02/task2.cpp
+++ HW02/task2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "convolution.h"
 #include <chrono>
+#include <random>
 
 using std::cout;
 using std::endl;
@@ -13,13 +14,18 @@ int main(int argc, char* argv[])
     high_resolution_clock::time_point start;
     high_resolution_clock::time_point end;
     duration<double, std::milli> duration_sec;
-    std::srand(static_cast<unsigned>(std::time(0)));
+    // std::srand(static_cast<unsigned>(std::time(0)));
+    std::random_device entropy_source;
+    std::mt19937 generator(entropy_source());
+    std::uniform_real_distribution<float> distn(-10., 10.);
+    std::uniform_real_distribution<float> distm(-1., 1.);
     float* image = new float[n * n];
 
     for (int i = 0; i < n * n; i++)
     {
-        image[i] = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 20.0)) - 10.0;
+        // image[i] = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 20.0)) - 10.0;
         // image[i] = i;
+        image[i] = distn(generator);
 
     }
 
@@ -27,9 +33,9 @@ int main(int argc, char* argv[])
     float* mask = new float[m * m];
     for (int i = 0; i < m * m; i++)
     {
-        mask[i] = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 2.0)) - 1.0;
+        // mask[i] = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 2.0)) - 1.0;
         // mask[i] = i;
-
+        mask[i] = distm(generator);
     }
 
     start = high_resolution_clock::now();
